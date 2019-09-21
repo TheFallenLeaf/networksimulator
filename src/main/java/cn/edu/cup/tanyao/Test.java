ef1f299ae523b1cb5eb2758a8e9d68da8a03b2db
tanyao <1029681537@qq.com>
2019-09-21 14:04:58 +0800

    Initialize the project configuration

---
+++ src/main/java/cn/edu/cup/tanyao/Test.java
@@ -0,0 +1,11 @@
+package cn.edu.cup.tanyao;
+
+/**
+ * @author tanyao
+ * @date 2019/9/21
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world !");
+    }
+}
